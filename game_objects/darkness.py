3adae827dc94571dec3ef7c56054f078f57a671a
aantr <xgamestudio8@gmail.com>
2021-05-31 15:21:39 +0500

    added darkness

--- game_objects/darkness.py
+++ game_objects/darkness.py
@@ -15,6 +15,8 @@ from utils.utils import bound, image2pil, pil2image, b2_coords
 
 
 class Darkness(GameObject):
+    layer = 2
+
     def __init__(self, world: b2World, cl: ContactListener, res: Resources,
                  player_car: PlayerCar, camera: Camera, game_object_group: Group, *groups):
         super().__init__(world, cl, game_object_group, *groups, connects=False)
@@ -28,13 +30,14 @@ class Darkness(GameObject):
         self.image = pygame.Surface((WIDTH, HEIGHT)).convert_alpha()
         self.image.fill((0, 0, 0, 0))
 
-        self.max_alpha = 100
+        self.color = 50, 50, 50
+        self.max_alpha = 220
         self.value = 0
         self.enabled = False
         self.timeout = 1.5
 
         # create circle
-        width = 400
+        width = 250
         border = 50
         circle = pygame.Surface((width, width)).convert_alpha()
         circle.fill((0, 0, 0, 0))
@@ -51,10 +54,11 @@ class Darkness(GameObject):
                       self.image.get_height() / 2 - self.circle.get_height() / 2
 
         self.ray_cast_timer = 0
-        self.ray_cast_timeout = 1 / 10
-        self.count_rays = 100
-        self.ray_cast_radius = int(((WIDTH // 2) ** 2 + (HEIGHT // 2) ** 2) ** 0.5) + 10
+        self.ray_cast_timeout = 1 / 3
+        self.count_rays = 25
+        self.ray_cast_radius = int(((WIDTH // 2) ** 2 + (HEIGHT // 2) ** 2) ** 0.5) + 200
         self.rays = [None for _ in range(self.count_rays)]
+        self.previous_camera_pos = None
 
     def set_center(self):
         cx, cy = self.camera.get_position() - self.player_car.get_position()
@@ -76,28 +80,46 @@ class Darkness(GameObject):
         k = 0
         for i in rays:
             if not i.reports:
-                self.rays[k] = b2_coords(i.end) * PPM
+                self.rays[k] = b2_coords(i.start) * PPM, b2_coords(i.end) * PPM
             else:
                 last_report: RayFixtureReport = i.reports[-1]
-                self.rays[k] = b2_coords(last_report.point) * PPM
+                self.rays[k] = b2_coords(i.start) * PPM, b2_coords(last_report.point) * PPM
             k += 1
 
+    def set_enabled(self, value):
+        self.enabled = value
+        if value:
+            self.get_rays()
+
     def update(self, dt: float, events):
+
+        for i in events:
+            if i.type == pygame.KEYDOWN:
+                if i.key == pygame.K_F10:
+                    self.set_enabled(not self.enabled)
+
         self.value += dt / self.timeout * (int(self.enabled) * 2 - 1)
         self.value = bound(self.value, 0, 1)
 
-        self.ray_cast_timer += dt
+        if self.value:
+            self.ray_cast_timer += dt
         if self.ray_cast_timer >= self.ray_cast_timeout:
             self.ray_cast_timer -= self.ray_cast_timeout
-            # self.get_rays()
+            self.get_rays()
 
     def render(self):
-        self.image.fill((0, 0, 0, self.value * self.max_alpha))
+        self.image.fill((*self.color, self.value * self.max_alpha))
         self.image.blit(self.circle, (self.center[0] - self.circle.get_width() / 2,
                                       self.center[1] - self.circle.get_height() / 2),
                         None, pygame.BLEND_RGBA_SUB)
-        # for i in self.rays:
-        #     if i is None:
-        #         continue
-        #     start, end = self.center, i + self.center - self.camera.get_position()
-        #     pygame.draw.line(self.image, (0, 0, 0, 0), start, end, width=3)
+        if self.value:
+            points = []
+            for i in self.rays:
+                if i is None:
+                    continue
+                start, end = self.camera.camera_shift * -1 + pygame.Vector2(
+                    *i[0]), self.camera.camera_shift * -1 + pygame.Vector2(*i[1])
+                # pygame.draw.line(self.image, (0, 0, 0, 0), start, end, width=3)
+                points.append(end)
+            if len(points) > 2:
+                pygame.draw.polygon(self.image, (0, 0, 0, 0), points)
